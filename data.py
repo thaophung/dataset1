f8c54af571ddfbe4ac910f1bb4429e12aaa13adc
Thao Phung <phungpthao@gmail.com>
2016-11-07 14:02:11 -0700

    random position + rotate

--- data.py
+++ data.py
@@ -10,11 +10,11 @@ def create_image(n):
     		font = ImageFont.truetype('Arial.ttf', 14)
 
     		# input image
-    		input_img = Image.new("RGB", size, (255, 255, 255))
+    		input_img = Image.new("RGB", size, (0, 0, 0))
     		draw_input = ImageDraw.Draw(input_img)
 
     		#label image
-    		label_img = Image.new("RGB", size, (255, 255, 255))
+    		label_img = Image.new("RGB", size, (0, 0, 0))
     		draw_label = ImageDraw.Draw(label_img)
 
     		# get random numbers
@@ -37,16 +37,18 @@ def create_image(n):
 		#num1 image
 		num1_x = random.randint(0,20)
 		num1_y = random.randint(0, 14)
-		num2_x = random.randint(49, 65)
+		num2_x = random.randint(50, 65)
 		num2_y = random.randint(0,14)
 		op_x = random.randint(27, 43)
+		
 		op_y = random.randint(0, 14) 
 		#draw_input.text((num1_x, num1_y), str(num1), (0,0,0),font=font)
-		draw_input.text((num1_x, num1_y), "3", (0,0,0), font=font)
-		#draw_input.text((op_x, op_y), operator, (0,0,0), font=font)
-		draw_input.text((op_x, op_y), "+", (0,0,0), font=font)
-		#draw_input.text((num2_x, num2_y), str(num2), (0,0,0), font=font)
-		draw_input.text((num2_x, num2_y), "7", (0,0,0), font=font)
+		draw_input.text((num1_x, num1_y), "3", (255,255,255), font=font)
+		
+		#draw_input.text((op_x, op_y), operator, (255,255,255), font=font)
+		draw_input.text((op_x, op_y), "+", (255,255,255), font=font)
+		#draw_input.text((num2_x, num2_y), str(num2), (), font=font)
+		draw_input.text((num2_x, num2_y), "7", (255,255,255), font=font)
 	
     		#label
     		#label_text = str(result)
@@ -57,7 +59,22 @@ def create_image(n):
     		draw_label.text((x_label, y_label), label_text, (0,0,0), font=font)
 		input_name = "input" + str(i) + ".png"    		
 		input_img.save(input_name)
+
+		#rotate
+		current_image = Image.open(input_name)
+		randRotate1 = random.randint(-45, 45)
+		crop_num1 = current_image.crop((0,0,28,28)).rotate(randRotate1)
+		#crop_num1.show()
+		current_image.paste(crop_num1, (0, 0))
+		current_image.save(input_name)
+		
+		randRotate2 = random.randint(-45, 45)
+		crop_num2 = current_image.crop((50, 0, 72, 28)).rotate(randRotate2)
+		current_image.paste(crop_num2, (50,0))
+		current_image.save(input_name)
+		#current_image.show()
 		#label_name = "label" + str(i) + ".png"
+
 
 		#label_img.save(label_name)
     
